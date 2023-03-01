f705992c12cd2cdc25017e1bd11a00fc8d3b6d11
DAAN <daanmgneni@gmail.com>
2023-03-01 17:13:02 +0530

    [Dhanesh] Add:shell script pgm of employee wage using switch case

--- employee.sh
+++ employee.sh
@@ -1,22 +1,29 @@
-  GNU nano 7.1                      employee.sh
-#To calculate the daily wage of an employee.
+#Solving using Switch Case statement 
 
 
-#! /bin/bash -x
+#!/bin/bash
 
-#variable
-emp_check=$((RANDOM%2)) # 0/1
+#variables
+random_check=$(( RANDOM % 3 )) # 0/1/2
 
-#constant varible
-is_present=1
+is_present_full_time=1
+is_present_part_time=2
 wage_per_hour=20
 
 #selection
-if [ $emp_check -eq $is_present ]
-then
-emp_hours=8
-salary=$(( emp_hours * wage_per_hour ))
-echo "Employee is present and daily wage is $salary"
-else
-salary=0
+case $random_check in
 
+$is_present_full_time) emp_hours=8
+echo " Employee is present for full time"
+;;
+
+$is_present_part_time) emp_hours=4
+echo " Employee is present for part time"
+;;
+*) emp_hours=0
+echo " Employee is absent"
+;;
+esac
+
+salary=$(( $emp_hours * $wage_per_hour))
+echo " Salary of an employee is $salary "

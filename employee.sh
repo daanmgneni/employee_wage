8904a6611a1803c42ca3583ef18755a0b7d915a9
DAAN <daanmgneni@gmail.com>
2023-03-01 17:03:15 +0530

    [Dhanesh] Add:Shell script pgm to calculate employee wage

--- employee.sh
+++ employee.sh
@@ -1,4 +1,4 @@
-#To check whether the employee is present or absent
+#To calculate the daily wage of an employee.
 
 
 #! /bin/bash -x
@@ -8,11 +8,15 @@ emp_check=$((RANDOM%2)) # 0/1
 
 #constant varible
 is_present=1
+wage_per_hour=20
 
 #selection
-if [ $emp_check -eq $is_present]
+if [ $emp_check -eq $is_present ]
 then
-echo "Employee is Present"
+emp_hours=8
+salary=$(( emp_hours * wage_per_hour ))
+echo "Employee is present and daily wage is $salary"
 else
-echo "Employee is Absent"
+salary=0
+"Employee is absent and daily wage is $salary"
 fi

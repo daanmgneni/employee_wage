fc21259933d5720a58d8664a8147d99c745a6a2c
DAAN <daanmgneni@gmail.com>
2023-03-01 17:36:29 +0530

    [Dhanesh] Add:Storing the daily wage along with the total wage

--- employee.sh
+++ employee.sh
@@ -1,4 +1,4 @@
-#Refactoring the code and writing the function to get the work hours of the employee.
+#Storing the daily wage along with the total wage
 
 
 #! /bin/bash -x
@@ -13,6 +13,9 @@ total_working_days=20
 #variable
 total_work_hour=0
 day=1
+
+# Declaring an array
+declare -a daily_wages
 
 # function to get the work hours of the employee
 function get_work_hours()
@@ -47,9 +50,14 @@ random_check=$((RANDOM%3)) # 0/1/2
 emp_hours=$(get_work_hours $random_check)
 
 total_work_hour=$(($total_work_hour + $emp_hours))
+
+# Storing the daily wage in the array
+daily_wages[$day]=$(( emp_hours * wage_per_hour))
+
 ((day++))
 
 done
 
 total_salary=$(( total_work_hour * wage_per_hour))
 echo "Total Salary of an Employee is $total_salary"
+echo "Storing the daily wage of an Employee : ${daily_wage[*]}"

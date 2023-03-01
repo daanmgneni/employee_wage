dfc9fdd74542487e8fecb879f5b6fe36f886add8
DAAN <daanmgneni@gmail.com>
2023-03-01 20:03:31 +0530

    [Dhanesh] Add:Shell script to calculate Part time wage

--- employee.sh
+++ employee.sh
@@ -1,22 +1,31 @@
-  GNU nano 7.1                      employee.sh
-#To calculate the daily wage of an employee.
+#Calculating wage for part time Employee.
 
 
-#! /bin/bash -x
+#!/bin/bash
 
 #variable
-emp_check=$((RANDOM%2)) # 0/1
+random_check=$(( RANDOM % 3 )) # 0/1/2
 
-#constant varible
-is_present=1
+is_present_full_time=1
+is_present_part_time=2
 wage_per_hour=20
-
-#selection
-if [ $emp_check -eq $is_present ]
-then
-emp_hours=8
-salary=$(( emp_hours * wage_per_hour ))
-echo "Employee is present and daily wage is $salary"
-else
+full_time_hour=8
+part_time_hour=4
 salary=0
 
+#selection
+if [ $random_check -eq $is_present_full_time ]
+then
+salary=$(( $full_time_hour * $wage_per_hour ))
+echo "Employee is present for full time"
+echo "Salary of the full time employee is $salary"
+
+elif [ $random_check -eq $is_present_part_time ]
+then
+salary=$(( $part_time_hour * $wage_per_hour ))
+echo "Employee is present for part time"
+echo "Salary of part time employee is $salary"
+
+else
+echo "Employee is absent and salary is $salary"
+fi

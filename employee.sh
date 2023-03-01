f7c11d962ab9d4833d96ff83ba3b63a653e84e90
DAAN <daanmgneni@gmail.com>
2023-03-01 17:24:05 +0530

    [Dhanesh] Add:Calculate wage until monthly condition is reached

--- employee.sh
+++ employee.sh
@@ -1,42 +1,47 @@
-#Calculating Wages For a month
+#Calculate wages till a condition of total working hours or days is reached for a month 
 
 
-#!/bin/bash
 
-#Constant variables
+#! /bin/bash -x
+
+#constant variable
 is_present_full_time=1
 is_present_part_time=2
 wage_per_hour=20
+max_work_hour=100
 total_working_days=20
 
-#Variable
+#variable
 total_work_hour=0
+day=1
 
-#Iterating FOR loop for 20 times, because total working days is 20
-for ((day=1 ;day<=$total_working_days; day++ ))
+#Loop will execute till both the condition is TRUE
+while (($day <= $total_working_days && $total_work_hour <= $max_work_hour ))
 do
-# 0/1/2
-random_check=$(( RANDOM % 3 ))
-case $random_check in
+#variable
+random_check=$((RANDOM%3)) # 0/1/2
 
-$is_present_full_time) emp_hours=8
+#selection
+case $random_check in
+$is_present_full_time)
+emp_hours=8
 echo " Employee is present for full time"
 ;;
-
-$is_present_part_time) emp_hours=4
+$is_present_part_time)
+emp_hours=4
 echo " Employee is present for part time"
 ;;
-*) emp_hours=0
+*)
+emp_hours=0
 echo " Employee is absent"
 ;;
 
 esac
+((day++))
 
-salary=$(( $emp_hours * $wage_per_hour))
-total_work_hour=$(( $total_work_hour + $emp_hours))
-echo " Salary of an employee is $salary "
+total_work_hour=$(( $total_work_hour + $emp_hours ))
+
 done
 
-# Calculating total salary for 20 days on the basis of employee hours
-total_salary=$(( $total_work_hour * $wage_per_hour))
-echo " Total Salary of an Employee is $total_salary "
+total_salary=$(( total_work_hour * wage_per_hour))
+echo "Total Salary of an Employee is $total_salary"
